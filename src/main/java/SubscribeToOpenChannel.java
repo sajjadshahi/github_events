51a3d13f36532ae9d821e7027cba023040e3f130
Sajjad Shahi <shahi_cse@yahoo.com>
2017-07-26 12:30:16 +0430

    Test SQL Filter

--- src/main/java/SubscribeToOpenChannel.java
+++ src/main/java/SubscribeToOpenChannel.java
@@ -2,11 +2,11 @@ import com.satori.rtm.*;
 import com.satori.rtm.model.*;
 
 
-
 public class SubscribeToOpenChannel {
     static final String endpoint = "wss://open-data.api.satori.com";
     static final String appkey = "783ecdCcb8c5f9E66A56cBFeeeB672C3";
     static final String channel = "github-events";
+    static final String filter = "select * from `github-events` where type='DeleteEvent'";
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -25,13 +25,15 @@ public class SubscribeToOpenChannel {
             public void onSubscriptionData(SubscriptionData data) {
                 for (AnyJson json : data.getMessages()) {
                     System.out.println("Got message: " + json.toString());
-                    GithubData sampleData1 = json.convertToType( GithubData.class );
-                    System.out.println(sampleData1.actor.url );
+                    GithubData sampleData1 = json.convertToType(GithubData.class);
+                    System.out.println(sampleData1.actor.url);
                 }
             }
         };
 
-        client.createSubscription(channel, SubscriptionMode.SIMPLE, listener);
+        SubscriptionConfig cfg = new SubscriptionConfig(SubscriptionMode.SIMPLE, listener)
+                .setFilter(filter);
+        client.createSubscription(channel, cfg);
 
         client.start();
     }
