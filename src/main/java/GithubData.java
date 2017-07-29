eb8c1839083abfec44d7698c4bffa8b913c953c0
ArtinTD <artin.taj@gmail.com>
2017-07-29 15:09:09 +0430

    adding repository to oop design

--- src/main/java/GithubData.java
+++ src/main/java/GithubData.java
@@ -5,4 +5,5 @@ public class GithubData {
     Actor actor;
     String id, type;
     PayLoad payload;
+    Repository repo;
 }
---
+++ src/main/java/Repository.java
@@ -0,0 +1,7 @@
+/**
+ * Created by ASUS on 7/29/2017.
+ */
+public class Repository {
+    int id;
+    String name, url;
+}
